c84807393108acdf3845a2cd9d310f22cadcf5c5
jan <jan@jan-Vostro-1700.(none)>
2013-02-10 19:46:41 -0800

    Modify StaticPages html.erb for test

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  describe "GET /static_pages" do
+    it "should have the content 'Sample App'" do
+    	    visit '/static_pages/home'
+    	    	page.should have_content('Sample App')    	   
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      get static_pages_index_path
+      response.status.should be(200)
+    end
+  end
+end
+
+   
