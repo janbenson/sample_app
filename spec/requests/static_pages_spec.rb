7267db0eed2276f9a880ef74b838ef2cafa69d0f
jan <jan@jan-Vostro-1700.(none)>
2013-02-10 20:40:33 -0800

    Modify StaticPages html.erb for test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,15 +1,42 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  describe "GET /static_pages" do
+	
+ describe "Home page" do
     it "should have the content 'Sample App'" do
     	    visit '/static_pages/home'
     	    	page.should have_content('Sample App')    	   
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get static_pages_index_path
-      response.status.should be(200)
     end
+    it "should have the right title" do
+    	visit '/static_pages/home'
+    	    page.should have_selector('title',
+                    :text => " | Home")
+  end
+  end
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_content('Help')
+    end
+    it "should have the right title" do
+    	visit '/static_pages/help'
+    	    page.should have_selector('title',
+                    :text => " | help")
   end
 end
+  describe "About page" do
 
-   
+    it "should have the content 'About'" do
+      visit '/static_pages/about'
+      page.should have_content('About Us')
+    end
+    it "should have the right title" do
+    	visit '/static_pages/about'
+    	    page.should have_selector('title',
+                    :text => " |About")
+  end
+end
+end
+
