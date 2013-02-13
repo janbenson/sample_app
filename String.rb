f57dd8b129d943b9873e20371693cc8a01cf7ab8
jan <jan@jan-Vostro-1700.(none)>
2013-02-12 20:01:45 -0800

    Finish layout and routes

---
+++ String.rb
@@ -0,0 +1,9 @@
+  	  person1 = { :first => "Father", :last => "Time" }
+  	  person2 = { :first => "Mother", :last => "Nature" }
+  	  person3 = { :first => "Child", :last => "OfGod" }
+  	  params = {}
+  	  params[:father] == person1
+  	  params[:mother] == person2
+  	  params[:child]  == person3
+
+  	  
